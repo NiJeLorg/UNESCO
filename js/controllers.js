6af8b2aa79a9b7e9dde1f2a035e6d527e8e024e9
JD Godchaux <jd@nijel.org>
2015-09-04 16:38:11 -0400

    Added legends and toggle between maps

--- js/controllers.js
+++ js/controllers.js
@@ -6,9 +6,22 @@
 $( document ).ready(function() {
 
 	// toggle map layer listeners
-	$( "input[name='OOSClayers']" ).change(function() {
-		activeLayer = $("input[name='OOSClayers']:checked").val();
-		drawOOSCMap();
+	$(".toggle").click(function() {
+		console.log($("#OOSCRatesMapToggleImage").attr("src"));
+		if ($("#OOSCRatesMapToggleImage").attr("src") == 'css/images/OOScToggle_Primary.png') {
+			$("#OOSCRatesMapToggleImage").attr("src", 'css/images/OOScToggle_LowerSecondary.png');
+			$("#leftToggleText").css("color", "#d1d2d3");
+			$("#rightToggleText").css("color", "#333");
+			activeLayer = 'ROFST_2_CP';
+			drawOOSCMap();
+		} else {
+			$("#OOSCRatesMapToggleImage").attr("src", 'css/images/OOScToggle_Primary.png');
+			$("#rightToggleText").css("color", "#d1d2d3");
+			$("#leftToggleText").css("color", "#333");
+			activeLayer = 'ROFST_1_CP';
+			drawOOSCMap();
+		}
+
 	});
 
 });
