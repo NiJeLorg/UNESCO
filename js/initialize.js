2aabd20b2a7aba7d7f1e17cfd7c6b64bf9f0c85f
kat hartman <kat.a.hartman@gmail.com>
2015-10-05 13:25:35 -0400

    updates

--- js/initialize.js
+++ js/initialize.js
@@ -7,7 +7,7 @@ getApiData();
 
 // function to get API data
 function getApiData() {
-	$.getJSON("http://www.uis.unesco.org/das/api/countries/?code=ALL&category=EDULIT_DS&indicators=ROFST_1_CP,ROFST_2_CP,OFST_1_CP,OFST_1_F_CP,OFST_1_M_CP,OFST_2_CP,OFST_2_F_CP,OFST_2_M_CP&fromyear=2009&toyear=2030&mostrecent=true&fullprecision=false&callback=?", function (rate) {
+	$.getJSON("http://www.uis.unesco.org/das/api/countries/?code=ALL&category=EDULIT_DS&indicators=ROFST_1_CP,ROFST_2_CP,OFST_1_F_CP,OFST_1_M_CP,OFST_2_F_CP,OFST_2_M_CP&fromyear=2009&toyear=2020&mostrecent=true&fullprecision=false&callback=?", function (rate){
 		apidata = rate;
 		// run FCEMap
 		createFCEMap();
